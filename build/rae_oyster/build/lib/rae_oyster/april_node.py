de9cd50b8399d0672dd67eab07470ca670693268
Joey <j.g.vanderkaaij@uva.nl>
2024-10-15 17:12:24 +0200

    april tag recognition WIP

--- build/rae_oyster/build/lib/rae_oyster/april_node.py
+++ build/rae_oyster/build/lib/rae_oyster/april_node.py
@@ -19,6 +19,8 @@ class ImageProcessor(Node):
         self.br = CvBridge()
 
         self.should_exit = False  # Control flag for exiting
+        self.tag_size = 1.0
+        self.camera_params = (600, 600, 4208/2, 3120/2)
 
         # Create a subscription to the camera topic
         self.publisher_image = self.create_publisher(Image, '/lcd', 10)
@@ -33,29 +35,80 @@ class ImageProcessor(Node):
 
 
     def image_callback(self, msg):
-        print('image received')
         np_arr = np.frombuffer(msg.data, np.uint8)
         # Convert ROS Compressed Image message to OpenCV2 format
-        current_frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-        cv2.imshow('Binary Threshold Control', current_frame)
-        #
-        # at_detector = Detector(
-        #     families="tag36h11",
-        #     nthreads=1,
-        #     quad_decimate=1.0,
-        #     quad_sigma=0.0,
-        #     refine_edges=1,
-        #     decode_sharpening=0.25,
-        #     debug=0
-        # )
+        color_frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        gray_frame = cv2.cvtColor(color_frame, cv2.COLOR_BGR2GRAY)
 
-        # at_detector.detect(current_frame)
+        at_detector = Detector(
+            families="tag36h11",
+            nthreads=1,
+            quad_decimate=1.0,
+            quad_sigma=0.0,
+            refine_edges=1,
+            decode_sharpening=0.25,
+            debug=0
+        )
+
+        tags = at_detector.detect(gray_frame, True, camera_params=self.camera_params, tag_size=self.tag_size)
+
+        for tag in tags:
+            print(tag.tag_id)
+            self._draw_pose(color_frame,
+                            self.camera_params,
+                            tag.pose_R,
+                            tag.pose_t)
+
+        cv2.imshow('April Tags', color_frame)
+
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             # If 'q' is pressed, exit the loop and close the window
             cv2.destroyAllWindows()
             self.should_exit = True  # Set the exit flag
 
+
+    def _draw_pose(self, overlay, cam_params, pose_r, pose_t):
+        opoints = np.array([
+            -1, -1, 0,
+            1, -1, 0,
+            1, 1, 0,
+            -1, 1, 0,
+            -1, -1, -2,
+            1, -1, -2,
+            1, 1, -2,
+            -1, 1, -2
+        ]).reshape(8, 3) * 0.5 * self.tag_size
+
+        edges = np.array([
+            0, 1,
+            1, 2,
+            2, 3,
+            3, 0,
+            0, 4,
+            1, 5,
+            2, 6,
+            3, 7,
+            4, 5,
+            5, 6,
+            6, 7,
+            7, 4
+        ]).reshape(-1, 2)
+
+        fx, fy, cx, cy = cam_params
+
+        K = np.array([fx, 0, cx, 0, fy, cy, 0, 0, 1], dtype=np.float32).reshape(3, 3)
+
+        dcoeffs = np.zeros(5)
+
+        ipoints, _ = cv2.projectPoints(opoints, pose_r, pose_t, K, dcoeffs)
+
+        ipoints = np.round(ipoints).astype(int)
+
+        ipoints = [tuple(pt) for pt in ipoints.reshape(-1, 2)]
+
+        for i, j in edges:
+            cv2.line(overlay, ipoints[i], ipoints[j], (0, 255, 0), 2, 16)
 
 def main(args=None):
     rclpy.init(args=args)
